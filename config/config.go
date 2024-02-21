868f08fd647763668f44d1a24a6dc26373545f04
Ricardo Guelfi <rfpart@gmail.com>
2024-02-21 15:43:54 -0300

    21-02-2024 15:43

--- config/config.go
+++ config/config.go
@@ -2,39 +2,50 @@ package configs
 
 import "github.com/spf13/viper"
 
-var cfg *config
+var cfg *config // cria a variável cfg que será o ponteiro de config, é privada para que apenas
+//este package possa acessar as configurações diretamente, mas que sempre que eu quiser acessar
+// as configurações eu não consiga alterar nada, eu faço a requisição e este package me retorna
+// as informações que foram configuradas neste arquivo.
 
 type config struct {
-	API APIConfig
-	DB  DBConfig
+	API APIConfig // servidor
+	DB  DBConfig  // banco de dados
 }
 
 type APIConfig struct {
-	Port string
+	Port string // porta que será usada para receber as requisições
 }
 
-type DBConfig struct {
-	Host     string
-	Port     string
-	User     string
-	Pass     string
-	Database string
-}
+type DBConfig struct { // esta struct vai unir as outras para fazer um unico arquivo mapeando
+	//cada dado dentro da sua struc e dentro de seu atributo
+	Host     string  // Host é o endereço do servidor de banco de dados.
+	Port     string  // Port é o número da porta do servidor de banco de dados.
+	User     string  // User é o nome de usuário para autenticação no banco de dados.
+	Pass     string  // Pass é a senha para autenticação no banco de dados.
+	Database string  // Database é o nome do banco de dados a ser acessado.
 
-func init() { // sempre é chamada no start das aplicações
-	viper.SetDefault("api.port", "9000")
-	viper.SetDefault("datavase.host", "localhost")
-	viper.SetDefault("database.port", "5432")
-}
 
-func load() error {
-	viper.SetConfigName("config")
-	viper.SetConfigType("toml")
-	viper.AddConfigPath(".")
-	err := viper.RedInConfig()
-	if err != nil {
-		if _, ok := err.viper(viper, ConfigFileNotFounfError); !ok {
-			return err
+}s
+
+func init() { // init sempre é chamada no start das aplicações (faz parte do ciclo de vida do Golang)
+
+	// viper é um pacote que será usado para definir valores padrões para nossas configurações.
+	viper.SetDefault("api.port", "9000") // define a chave api.port, e a porta padrao 9000 (porta onde a API fica escutando por requisições)  
+	viper.SetDefault("datavase.host", "localhost") // Define o valor padrão para o host do banco de dados como "localhost"
+    viper.SetDefault("database.port", "5432")      // Define o valor padrão para a porta do banco de dados como "5432"
+
+
+func load() error {  // retorna com error caso não consiga carregar as configurações
+	viper.SetConfigName("config") // aqui o viper vai procurar pelo arquivo de nome "config"
+	viper.SetConfigType("toml") // Define o tipo de configuração como TOML (Tom's Obvious, Minimal Language)
+	viper.AddConfigPath(".") // Define o diretório atual como o local para procurar arquivos de configuração
+
+err := viper.ReadInConfig() // o viper vai ler o arquivo de configuração e guardar o resultado em err
+if err != nil { // Verifica se o erro é diferente de nil(sem erro) para fazer o if abaixo, se for nill continua depois do return.   
+    if _, ok := err.viper(viper, ConfigFileNotFoundError); !ok { // Verifica se o erro não é do tipo ConfigFileNotFoundError.     
+        return err // Retorna o erro.
+    }
+}
 		}
 
 	}
