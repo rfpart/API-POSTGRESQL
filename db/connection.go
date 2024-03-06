1c0375e1ffb24c4b344f1c360e8fef25ad047421
Ricardo Guelfi <rfpart@gmail.com>
2024-03-06 13:44:33 -0300

    06-03-2024

--- db/connection.go
+++ db/connection.go
@@ -1,25 +1,25 @@
 package db // este pacote é responsável pela conexão com banco de dados
 
 import (
-	"database/sql"
+	"database/sql" //pacote usado em go para se conectar ao PostgreSQL e excutar consultas
 	"fmt"
 
-	configs "github.com/API-POSTGRESQL/config"
-	_ "github.com/lib/pq"
+	"github.com/API-POSTGRESQL/configs" // importa o pacote de configuração do banco de dados
+	_ "github.com/lib/pq"               //Driver que permite conectar aplicativos go ao banco de dados PortgreSQL
 )
 
 func OpenConnection() (*sql.DB, error) { // esta função abre a conexão com banco de dados
 	conf := configs.GetDb() // a variável conf recebe as configurações do banco de dados para poder conectar
 
 	sc := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		conf.Host, conf.Port, conf.User, conf.Pass, conf.Database)
+		conf.Host, conf.Port, conf.User, conf.Pass, conf.Database) // cria uma string de conexão com os dados armazenados na struct DBConfig que está no pakage config
 
-	conn, err := sql.Open("postgres", sc)
+	conn, err := sql.Open("postgres", sc) // sql.Open() abre uma nova conexão com o banco de dados PostgreSQL usando o driver "postgres"
 	if err != nil {
-		panic(err)
+		panic(err) // se houver um erro ao abrir a conexão, o programa será encerrado com uma mensagem de erro
 	}
 
-	err = conn.Ping() //
+	err = conn.Ping() // Ping() verifica se a conexão com o banco de dados está funcionando corretamente
 
-	return conn, err
+	return conn, err // retorna a conexão com o banco de dados e qualquer erro que possa ter ocorrido
 }
