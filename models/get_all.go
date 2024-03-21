c78ddcbc4b31a5181e345581ce96958a0a16b3fd
Ricardo Guelfi <rfpart@gmail.com>
2024-03-21 06:19:11 -0300

    21-03-2024 06:18

--- models/get_all.go
+++ models/get_all.go
@@ -1,25 +1,35 @@
-package models
+package models // Pacote models para agrupar estruturas de dados relacionadas
 
-import "github.com/API-POSTGRESQL/db"
+import "github.com/API-POSTGRESQL/db" // Importa o pacote db para interagir com o banco de dados
 
+// GetAll retorna todas as tarefas do banco de dados
 func GetAll() (todos []Todo, err error) {
+	// Abre uma conexão com o banco de dados
 	conn, err := db.OpenConnection()
 	if err != nil {
-		return
+		return // Retorna um erro se não for possível abrir a conexão
 	}
-	defer conn.Close()
+	defer conn.Close() // Fecha a conexão quando a função terminar, independentemente do resultado
+
+	// Executa a consulta SQL para selecionar todas as tarefas
 	rows, err := conn.Query(`SELECT * FROM todos`)
 	if err != nil {
-		return
+		return // Retorna um erro se não for possível executar a consulta
 	}
+	defer rows.Close() // Fecha o cursor quando a função terminar, independentemente do resultado
+
+	// Loop sobre os resultados da consulta
 	for rows.Next() {
 		var todo Todo
 
+		// Escaneia os valores retornados pela consulta e atribui-os à estrutura todo
 		err = rows.Scan(&todo.ID, &todo.Title, &todo.Description, &todo.Done)
 		if err != nil {
-			continue
+			continue // Pula para a próxima iteração do loop se ocorrer um erro ao escanear os valores
 		}
-		todos = append(todos, todo)
+
+		todos = append(todos, todo) // Adiciona a tarefa à lista de tarefas recuperadas
 	}
-	return
+
+	return // Retorna todas as tarefas recuperadas e qualquer erro que ocorra durante a execução da consulta
 }
