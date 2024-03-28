1699fb46a7227950a5fad8203e19ce75e714346c
Ricardo Guelfi <rfpart@gmail.com>
2024-03-28 07:19:55 -0300

    28-03-2024 07:19

--- models/delete.go
+++ models/delete.go
@@ -1,18 +1,23 @@
-package models
+package models // Pacote models para agrupar estruturas de dados relacionadas
 
-import "github.com/API-POSTGRESQL/db"
+import "github.com/API-POSTGRESQL/db" // Importa o pacote db para interagir com o banco de dados
 
+// Delete exclui uma tarefa do banco de dados com o ID fornecido
 func Delete(id int64) (int64, error) {
-	conn, err := db.OpenConnection()
-	if err != nil {
-		return 0, err
-	}
-	defer conn.Close()
-	res, err := conn.Exec(`DELETE FROM todos WHERE id=$1`, id)
-	if err != nil {
-		return 0, err
-	}
+    // Abre uma conexão com o banco de dados
+    conn, err := db.OpenConnection()
+    if err != nil {
+        return 0, err // Retorna 0 e o erro se não for possível abrir a conexão
+    }
+    defer conn.Close() // Fecha a conexão quando a função terminar, independentemente do resultado
 
-	return res.RowsAffected()
+    // Executa a exclusão no banco de dados com a instrução SQL fornecida
+    res, err := conn.Exec(`DELETE FROM todos WHERE id=$1`, id)
+    if err != nil {
+        return 0, err // Retorna 0 e o erro se ocorrer um erro durante a execução da exclusão
+    }
+
+    // Retorna o número de linhas afetadas pela exclusão
+    return res.RowsAffected()
 }
 
