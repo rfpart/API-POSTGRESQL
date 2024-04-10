62843dfab9d8cdb046ccd85b7791c5cf8bb9e90c
Ricardo Guelfi <rfpart@gmail.com>
2024-04-10 07:34:14 -0300

    10-04-2024 07:34

--- main.go
+++ main.go
@@ -4,22 +4,28 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/API-POSTGRESQL/handlers"
-	"github.com/aprendagolang/api-pgsql/configs"
-	"github.com/go-chi/chi/v5"
+	"github.com/API-POSTGRESQL/handlers"         // Importa os manipuladores de solicitação HTTP
+	"github.com/aprendagolang/api-pgsql/configs" // Importa as configurações do servidor
+	"github.com/go-chi/chi/v5"                   // Importa o roteador de solicitações HTTP
 )
 
 func main() {
+	// Carrega as configurações do servidor
 	err := configs.Load()
 	if err != nil {
-		panic(err)
+		panic(err) // Em caso de erro ao carregar as configurações, encerra o programa
 	}
 
+	// Cria um novo roteador Chi
 	r := chi.NewRouter()
-	r.Post("/", handlers.Create)
-	r.Put("/{id}", handlers.Update())
-	r.Delete("/{id}", handlers.Delete)
-	r.Get("/", handlers.List)
-	r.Get("/{id}", handlers.Get)
+
+	// Define as rotas e seus manipuladores correspondentes
+	r.Post("/", handlers.Create)       // Rota para criar um novo recurso
+	r.Put("/{id}", handlers.Update())  // Rota para atualizar um recurso existente
+	r.Delete("/{id}", handlers.Delete) // Rota para excluir um recurso existente
+	r.Get("/", handlers.List)          // Rota para obter todos os recursos
+	r.Get("/{id}", handlers.Get)       // Rota para obter um recurso específico
+
+	// Inicia o servidor na porta especificada nas configurações
 	http.ListenAndServe(fmt.Sprintf(":%s", configs.GetServerPort()), r)
 }
